3678a3548359b5bd6f7e93b128878fe39b7b8e0c
shakil <mask.shakill@gmail.com>
2025-04-28 01:06:32 +0600

    test simple route calcualtion

--- server.ts
+++ server.ts
@@ -1,10 +1,27 @@
 import express, { Request, Response } from "express";
 
+type Revenue = number;
+type Expenses = number;
+type Profit = number;
+
+const calculateNetProfit = (revenue: Revenue, expenses: Expenses): Profit => {
+  return revenue - expenses;
+};
+
 const app = express();
 const port = 3000;
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/calculate", (_req: Request, res: Response) => {
+  const revenue: Revenue = 100000;
+  const expenses: Expenses = 75000;
+
+  const netProfit: Profit = calculateNetProfit(revenue, expenses);
+
+  res.json({
+    revenue,
+    expenses,
+    netProfit,
+  });
 });
 
 app.listen(port, () => {
